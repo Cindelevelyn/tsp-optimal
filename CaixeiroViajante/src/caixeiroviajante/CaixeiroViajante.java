dfef20ac5ca24d57d3273d75e73de720fac6b7a6
Cindel Sousa <60244854+Cindelevelyn@users.noreply.github.com>
2022-10-12 20:26:04 -0300

    Add files via upload

--- CaixeiroViajante/src/caixeiroviajante/CaixeiroViajante.java
+++ CaixeiroViajante/src/caixeiroviajante/CaixeiroViajante.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
  */
 public class CaixeiroViajante {
 
+    private static int menor = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
 
         FileManager fileManager = new FileManager();
@@ -40,32 +42,77 @@ public class CaixeiroViajante {
             }
         }
 
-        grafo.printGraph();
-
         int vetorCaminho[] = new int[nVertex];
+        int caminho[] = new int[nVertex];
 
         boolean[] av = new boolean[nVertex];
         for (int i = 0; i < nVertex; i++) {
             av[i] = true;
+            vetorCaminho[i] = -1;
         }
 
-        backTracking(grafo, av, 0, vetorCaminho, 0);
+        vetorCaminho[0] = 0;
+        av[0] = false;
+
+        long inicio = System.currentTimeMillis();
+
+        backTracking(grafo, av, 0, vetorCaminho, 1, caminho);
+
+        System.out.print("O menor caminho é: [");
+        for (int i = 0; i < nVertex; i++) {
+            System.out.print(caminho[i]);
+            if(i < nVertex-1)
+            {
+                System.out.print(" ");
+            }
+        }
+        // Runtime rt = Runtime.getRuntime();
+        System.out.println("]\nSeu custo é de: " + menor);
+    
+        long tempo = System.currentTimeMillis() - inicio;
+
+        System.out.println("\nO tempo de execução foi de: " + tempo + "ms");
+
+        // long x = rt.totalMemory() - rt.freeMemory();
+        // System.out.println("\nA memória utilizada foi de: " + x);
     }
 
-    public static void backTracking(Graph grafo, boolean av[], int node, int vetorCaminho[], int prof) {
+    public static void backTracking(Graph grafo, boolean av[], int node, int vetorCaminho[], int prof, int[] caminho) {
+
+        int menorCaminhoAux = 0;
+
+        /* operacao de parada */
         if (prof >= grafo.getVertexSize()) {
-            for (int i = 0; i < prof; i++) {
-                System.out.print(vetorCaminho[i] + "\t");
+            for (int i = 0, j = 0; i < prof; i++, j++) {
+                // System.out.print(vetorCaminho[i] + "\t");
+
+                if (j < prof - 1) {
+                    menorCaminhoAux += grafo.getPeso(vetorCaminho[j], vetorCaminho[j + 1]);
+                    // System.out.println(menorCaminhoAux);
+                }
+
+                /* operação menor caminho */
+                if (j == prof - 1) {
+                    menorCaminhoAux += grafo.getPeso(vetorCaminho[j], vetorCaminho[j - (prof-1)]);
+                    // System.out.println("custo: " + menorCaminhoAux);
+                    if (menorCaminhoAux <= menor) {
+                        for(int k = 0; k < prof; k++)
+                        {
+                            caminho[k] = vetorCaminho[k];
+                        }
+                        menor = menorCaminhoAux;
+                    }
+                }
             }
-            System.out.println("");
+            // System.out.println("");
         } else {
             ArrayList<Integer> adj = grafo.getAdjVertex(node);
             for (int i : adj) {
                 if (av[i] == true) {
                     av[i] = false;
-                    vetorCaminho[prof] = node;
+                    vetorCaminho[prof] = i;
                     prof++;
-                    backTracking(grafo, av, i, vetorCaminho, prof);
+                    backTracking(grafo, av, i, vetorCaminho, prof, caminho);
                     prof--;
                     av[i] = true;
                 }
--- CaixeiroViajante/src/caixeiroviajante/CaixeiroViajante2.java
+++ CaixeiroViajante/src/caixeiroviajante/CaixeiroViajante2.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
  */
 public class CaixeiroViajante2 {
 
+    private static int menor = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
 
         FileManager fileManager = new FileManager();
@@ -35,51 +37,60 @@ public class CaixeiroViajante2 {
                      * ADICIONAR A ARESTA À REPRESENTAÇÃO
                      */
                     grafo.setEdge(oriVertex, targetVertex, weight);
-                    grafo.setEdge(targetVertex, oriVertex, weight);
+                    // grafo.setEdge(targetVertex, oriVertex, weight);
                 }
             }
         }
 
         int vetorCaminho[] = new int[nVertex];
+        int caminho[] = new int[nVertex];
 
         boolean[] av = new boolean[nVertex];
         for (int i = 0; i < nVertex; i++) {
             av[i] = true;
+            vetorCaminho[i] = -1;
         }
 
-        int menor = Integer.MAX_VALUE;
-        int l = 0;
+        vetorCaminho[0] = 0;
+        av[0] = false;
+
+        backTracking(grafo, av, 0, vetorCaminho, 1, caminho);
+
+        System.out.print("O menor caminho é: [");
         for (int i = 0; i < nVertex; i++) {
-            l += i;
+            System.out.print(caminho[i]);
+            if(i < nVertex-1)
+            {
+                System.out.print(" ");
+            }
         }
-
-        backTracking(grafo, av, 0, vetorCaminho, 0, l, menor);
+        System.out.println("]\nSeu custo é de: " + menor);
     }
 
-    public static void backTracking(Graph grafo, boolean av[], int node, int vetorCaminho[], int prof, int l,
-            int menor) {
+    public static void backTracking(Graph grafo, boolean av[], int node, int vetorCaminho[], int prof, int[] caminho) {
 
         int menorCaminhoAux = 0;
-        int caminhoTotal = 0;
-
-        /* comparação de caminho aceitavel */
-        for (int i = 0; i < grafo.getVertexSize(); i++) {
-            caminhoTotal += vetorCaminho[i];
-        }
 
         /* operacao de parada */
-        if (prof >= grafo.getVertexSize() && caminhoTotal == l) {
+        if (prof >= grafo.getVertexSize()) {
             for (int i = 0, j = 0; i < prof; i++, j++) {
                 System.out.print(vetorCaminho[i] + "\t");
-                if (j < prof - 1)
+
+                if (j < prof - 1) {
                     menorCaminhoAux += grafo.getPeso(vetorCaminho[j], vetorCaminho[j + 1]);
-                System.out.println(menorCaminhoAux);
+                    // System.out.println(menorCaminhoAux);
+                }
 
                 /* operação menor caminho */
                 if (j == prof - 1) {
-                    if (menorCaminhoAux < menor) {
+                    menorCaminhoAux += grafo.getPeso(vetorCaminho[j], vetorCaminho[j - (prof-1)]);
+                    System.out.println("custo: " + menorCaminhoAux);
+                    if (menorCaminhoAux <= menor) {
+                        for(int k = 0; k < prof; k++)
+                        {
+                            caminho[k] = vetorCaminho[k];
+                        }
                         menor = menorCaminhoAux;
-                        System.out.println("menor: " + menor);
                     }
                 }
             }
@@ -89,9 +100,9 @@ public class CaixeiroViajante2 {
             for (int i : adj) {
                 if (av[i] == true) {
                     av[i] = false;
-                    vetorCaminho[prof] = node;
+                    vetorCaminho[prof] = i;
                     prof++;
-                    backTracking(grafo, av, i, vetorCaminho, prof, l, menor);
+                    backTracking(grafo, av, i, vetorCaminho, prof, caminho);
                     prof--;
                     av[i] = true;
                 }
